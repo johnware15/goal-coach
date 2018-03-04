5f1e257d74cf190b0b6db99222a8c9f6f2b498f4
johnware15 <contact@johndware.com>
2018-03-03 18:17:11 -0800

    Build src directory

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+import SignIn from './components/signIn'
+import SignUp from './components/signUp'
+
+ReactDOM.render(
+  <div>App</div>, document.getElementById('root')
+)
